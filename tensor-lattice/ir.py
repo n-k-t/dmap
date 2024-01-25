511bbac381638da991aa5777de890be4ffe240e0
Nathaniel Tomczak <nkt8@case.edu>
2024-01-25 14:54:59 -0500

    Working on loop and indexing IR for binary operations.

--- tensor-lattice/ir.py
+++ tensor-lattice/ir.py
@@ -15,21 +15,48 @@ class IR:
         self.value: int | float | None = value
         self.dependencies: list[IR] = dependencies
 
-def bin_op_to_ir(child: Tensor, kernel: list[IR], tensors: dict[Tensor, IR], consts: dict[int | float, IR]) -> None:
-    for i in child._parents[0]._memory.view:
-        if i != 1:
-            loop_to_ir(lower_bound = 0, upper_bound = i, kernel = kernel, consts = consts)
+    def __repr__(self) -> str:
+        return f"OP: {self.op:>10},    DT: {self.data_type:>10},    VAL: {str(self.value):>10},    DEP: {[(i.op, i.value) for i in self.dependencies]}"
 
-def loop_to_ir(lower_bound: int, upper_bound: int, kernel: list[IR], consts: dict[int | float, IR]) -> None:
+def bin_op_to_ir(child: Tensor, kernel: list[IR], tensors: dict[Tensor, IR], consts: dict[int | float, IR]) -> None:
+    axis_loops: dict[int, IR] = {}
+
+    for num, i in enumerate(child._parents[0]._memory.view):
+        if i != 1:
+            loop_to_ir(lower_bound = 0, upper_bound = i, name = f"index_{num}", kernel = kernel, consts = consts)
+            axis_loops[num] = kernel[-1]
+
+    store_add: IR | None = None
+
+    for tensor in child._parents:
+        for index, axis in enumerate(axis_loops):
+            if axis not in consts:
+                temp_bound: IR = IR(op = "CONST", data_type = "int", value = axis, dependencies = [])
+                consts[axis] = temp_bound
+                kernel.append(temp_bound)
+            if tensor._memory.stride[index] not in consts:
+                temp_bound: IR = IR(op = "CONST", data_type = "int", value = tensor._memory.stride[index], dependencies = [])
+                consts[tensor._memory.stride[index]] = temp_bound
+                kernel.append(temp_bound)
+            temp_op: IR = IR(op = "MUL", data_type = "int", value = "", dependencies = [axis_loops[axis], consts[tensor._memory.stride[index]]])
+            kernel.append(temp_op)
+            if index != 0:
+                temp_op: IR = IR(op = "ADD", data_type = "int", value = "", dependencies = [store_add, temp_op])
+                kernel.append(temp_op)
+            store_add = temp_op
+        temp_load: IR = IR(op = "LOAD", data_type = "float", value = "", dependencies = [tensors[tensor], kernel[-1]])
+        kernel.append(temp_load)
+
+def loop_to_ir(lower_bound: int, upper_bound: int, name: str, kernel: list[IR], consts: dict[int | float, IR]) -> None:
     if lower_bound not in consts:
         temp_bound: IR = IR(op = "CONST", data_type = "int", value = lower_bound, dependencies = [])
-        consts[0] = temp_bound
+        consts[lower_bound] = temp_bound
         kernel.append(temp_bound)
     if upper_bound not in consts:
         temp_bound: IR = IR(op = "CONST", data_type = "int", value = upper_bound, dependencies = [])
-        consts[0] = temp_bound
+        consts[upper_bound] = temp_bound
         kernel.append(temp_bound)
-    temp_loop: IR = IR(op = "FOR", data_type = "", value = "", dependencies = [consts[lower_bound], consts[upper_bound]])
+    temp_loop: IR = IR(op = "FOR", data_type = "", value = name, dependencies = [consts[lower_bound], consts[upper_bound]])
     kernel.append(temp_loop)
 
 def to_ir(end: Tensor) -> list[IR]:
@@ -42,15 +69,20 @@ def to_ir(end: Tensor) -> list[IR]:
 
     for num, i in enumerate(tensor_list):
         if isinstance(i._op, MemoryOp):
-            temp: IR = IR(op = "ARG", data_type = "float*", value = f"tensor_{num}", dependencies = [])
-            kernel_ir.append(temp)
-            tensor_pointers[i] = temp
+            if num == len(tensor_list) - 1:
+                temp: IR = IR(op = "ARG", data_type = "float*", value = f"tensor_out", dependencies = [])
+                kernel_ir.append(temp)
+                tensor_pointers[i] = temp
+            else:
+                temp: IR = IR(op = "ARG", data_type = "float*", value = f"tensor_{num}", dependencies = [])
+                kernel_ir.append(temp)
+                tensor_pointers[i] = temp
         elif isinstance(i._op, BinaryOp):
+            if num == len(tensor_list) - 1:
+                temp: IR = IR(op = "ARG", data_type = "float*", value = f"tensor_out", dependencies = [])
+                kernel_ir.append(temp)
+                tensor_pointers[i] = temp
             op_tensors[i] = i._op
-        if num == len(tensor_list) - 1:
-            temp: IR = IR(op = "ARG", data_type = "float*", value = f"tensor_out", dependencies = [])
-            kernel_ir.append(temp)
-            tensor_pointers[i] = temp
 
     for child in op_tensors:
         bin_op_to_ir(child = child, kernel = kernel_ir, tensors = tensor_pointers, consts = const_pointers)
