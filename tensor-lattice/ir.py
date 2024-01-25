12bc1d07d8adaf317512fc96848b5c7ed8b835a8
n-k-t <nkt8@case.edu>
2024-01-25 08:01:09 -0500

    Adding in support for 'for loops' in the IR.

--- tensor-lattice/ir.py
+++ tensor-lattice/ir.py
@@ -15,12 +15,30 @@ class IR:
         self.value: int | float | None = value
         self.dependencies: list[IR] = dependencies
 
+def bin_op_to_ir(child: Tensor, kernel: list[IR], tensors: dict[Tensor, IR], consts: dict[int | float, IR]) -> None:
+    for i in child._parents[0]._memory.view:
+        if i != 1:
+            loop_to_ir(lower_bound = 0, upper_bound = i, kernel = kernel, consts = consts)
+
+def loop_to_ir(lower_bound: int, upper_bound: int, kernel: list[IR], consts: dict[int | float, IR]) -> None:
+    if lower_bound not in consts:
+        temp_bound: IR = IR(op = "CONST", data_type = "int", value = lower_bound, dependencies = [])
+        consts[0] = temp_bound
+        kernel.append(temp_bound)
+    if upper_bound not in consts:
+        temp_bound: IR = IR(op = "CONST", data_type = "int", value = upper_bound, dependencies = [])
+        consts[0] = temp_bound
+        kernel.append(temp_bound)
+    temp_loop: IR = IR(op = "FOR", data_type = "", value = "", dependencies = [consts[lower_bound], consts[upper_bound]])
+    kernel.append(temp_loop)
+
 def to_ir(end: Tensor) -> list[IR]:
     kernel_ir: list[IR] = []
 
     tensor_list: list[Tensor] = end._topological_sort()
-    op_list: list[BinaryOp] = []
+    op_tensors: dict[Tensor, BinaryOp] = {}
     tensor_pointers: dict[Tensor, IR] = {}
+    const_pointers: dict[int | float, IR] = {}
 
     for num, i in enumerate(tensor_list):
         if isinstance(i._op, MemoryOp):
@@ -28,12 +46,13 @@ def to_ir(end: Tensor) -> list[IR]:
             kernel_ir.append(temp)
             tensor_pointers[i] = temp
         elif isinstance(i._op, BinaryOp):
-            op_list.append(i._op)
+            op_tensors[i] = i._op
         if num == len(tensor_list) - 1:
             temp: IR = IR(op = "ARG", data_type = "float*", value = f"tensor_out", dependencies = [])
             kernel_ir.append(temp)
             tensor_pointers[i] = temp
 
-    
+    for child in op_tensors:
+        bin_op_to_ir(child = child, kernel = kernel_ir, tensors = tensor_pointers, consts = const_pointers)
     
     return kernel_ir
