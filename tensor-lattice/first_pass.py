32ec9cbd79962159fdeacb5423a28aa8dcd0896e
n-k-t <nkt8@case.edu>
2024-02-01 08:40:43 -0500

    Small changes on the new first pass.

--- tensor-lattice/first_pass.py
+++ tensor-lattice/first_pass.py
@@ -20,14 +20,17 @@ class IR:
         return f"OP: {self.op:>10},    DT: {self.data_type:>10},    VAL: {str(self.value):>10},    DEP: {[(i.op, i.value) for i in self.dependencies]}"
 
 
-def separate_kernels(end: Tensor) -> list[Tensor]:
+def separate_kernels(end: Tensor) -> list[list[Tensor]]:
     ast: list[Tensor] = end._topological_sort()
 
-    kernel_tensors: list[Tensor] = []
+    kernel_tensors: list[list[Tensor]] = []
+    temp_tensors: list[Tensor] = []
 
     for tensor in ast:
+        temp_tensors.append(tensor)
         if isinstance(tensor._op, BinaryOp):
-            kernel_tensors.append(tensor)
+            kernel_tensors.append(deepcopy(temp_tensors))
+            temp_tensors: list[Tensor] = []
 
     return kernel_tensors
 
@@ -50,22 +53,28 @@ def indexing_ir(tensor: Tensor, kernel: list[IR], dimensions: list[IR], tensor_p
     kernel.append(temp_load)
 
 
-def preliminary_ir(head: Tensor) -> list[IR]:
+def preliminary_ir(ast_slice: list[Tensor]) -> list[IR]:
     kernel: list[IR] = []
     tensor_pointers: dict[Tensor, IR] = {}
+    arg_counter = 0
 
     # Maybe as things are created, I can label them as load/store. This could help create directives down the line.
     # Further optimizations can be added that remove stores and replace them with loads if shapes match up/same operand.
     # Could be a curated second AST that indicates loads/stores and points to them. There can be overlap -> fusion in the future.
     #### I think I should alter this so that we take in AST slices instead of just a node. That way we can trace movement ops when 
     #### fusion does occur.
-    for num, parent in enumerate(head._parents):
-        temp: IR = IR(op = "ARG", data_type = parent._memory._data_type, value = f"operand_{num}", dependencies = [])
-        tensor_pointers[parent] = temp
-        kernel.append(temp)
-    temp: IR = IR(op = "ARG", data_type = head._memory._data_type, value = "result", dependencies = [])
-    tensor_pointers[head] = temp
-    kernel.append(temp)
+    for tensor in ast_slice:
+        if tensor._parents[0] in tensor_pointers:
+            tensor_pointers[tensor] = tensor_pointers[tensor._parents[0]]
+        elif isinstance(tensor._op, BinaryOp):
+            for parent in tensor._parents:
+                temp: IR = IR(op = "ARG", data_type = parent._memory._data_type, value = f"operand_{arg_counter}", dependencies = [])
+                tensor_pointers[parent] = temp
+                kernel.append(temp)
+                arg_counter += 1
+            temp: IR = IR(op = "ARG", data_type = tensor._memory._data_type, value = "result", dependencies = [])
+            tensor_pointers[tensor] = temp
+            kernel.append(temp)
 
     global_shape: list[int] = head._parents[0]._memory.view
     dimensions: list[IR] = []
