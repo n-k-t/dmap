0ac58582b21fd26a01e545936857035291475d2f
n-k-t <nkt8@case.edu>
2024-03-05 15:50:45 -0500

    Added a test dependency.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open(file = './README.md', mode = 'r') as f:
+    description = f.read()
+
 setup(
     name = 'dmap',
     version = '0.0.1',
-    description = 'A dimensional mapping tool.',
     author = 'Nat Tomczak',
+    description = 'A dimensional mapping tool.',
+    long_description = description,
+    long_description_content_style = 'text/markdown',
     license = 'MIT',
     packages = ['dmap'],
     python_requires = '>=3.7',
     install_requires = [], 
-    extras_require = {}
+    extras_require = {'tests': ['pytest']}
 )
